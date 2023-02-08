b9041038ae37046a83cc6c1cbec9b0d1c48e8b5a
Mehmet Akif Duba <mail@makifdb.com>
2023-02-09 01:17:49 +0300

    fix request body

--- assets/js/requests/enkaz.js
+++ assets/js/requests/enkaz.js
@@ -6,10 +6,22 @@ form.onsubmit = function (e) {
     e.preventDefault();
 
     var formData = new FormData(form);
+
+    // convert formData to JSON
+    var object = {};
+    
+    formData.forEach(function (value, key) {
+        object[key] = value;
+    });
+
+    var json = JSON.stringify(object);
     
     fetch(API_URL + "yardim", {
         method: "POST",
-        body: formData
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: json,
     }).then(function (response) {
         return response.json();
     }).then(function (data) {
