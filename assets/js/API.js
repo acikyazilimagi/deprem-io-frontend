cbab5b85de5a389ad703325befb4c170493555f9
karasakalmt <karasakal99@gmail.com>
2023-02-09 23:41:46 +0300

    iletisim form

--- assets/js/API.js
+++ assets/js/API.js
@@ -1,2 +1,3 @@
-//export const API_URL = "https://deprem-27jjydhzba-ew.a.run.app/";
-export const API_URL = "http://localhost:8080/";
+export const API_URL = "https://deprem-27jjydhzba-ew.a.run.app/";
+exports.API_URL;
+// export const API_URL = "http://localhost:8080/";
